a2cb125776abde32acc8e193eb2fbcf1ebaaeb36
Felipe Fernandes <ripcorp@gmail.com>
2015-11-13 23:54:09 -0200

    Formulário de inserção popup

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
 
-  $.ajax({
-      type: "GET",
-      url: 'https://sheetsu.com/apis/d12adf03',
-      success: processarDadosPlanilha
-  });
+  // Função de inicio para carregar os dados vindo so Sheetsu
+  function startUp() {
+    $.ajax({
+        type: "GET",
+        url: 'https://sheetsu.com/apis/d12adf03',
+        success: processarDadosPlanilha,
+        complete: function() {
+          $('.loading').fadeOut();
+        }
+    });
+  }
+
   // Função ajax que será chamada quando os dados forem recebidos do Sheetsu
   // Caso a requisição tenha ocorrido com sucesso, faça:
   function processarDadosPlanilha(data){
@@ -13,7 +20,7 @@ $(document).ready(function(){
       $count = 0;
 
       jQuery.each(data.result, function(chave, valor) {
-        $linha += "<tr>";
+        $linha += "<tr class='linha'>";
         $.each(valor, function(chave_interna, valor_interna) {
           //console.log( chave_interna + " : " + valor_interna);
           if ((chave_interna == "Onde")   ||
@@ -26,61 +33,94 @@ $(document).ready(function(){
           }
         });
         $linha += "</tr>"
-      });      
-
-      //$linha = "<td colspan='5'>Olá</td>";
-
-
+      });
       $('#dados_locais tbody').append( $linha );
-
-
-
-
   }
 
+  // Função para atualizar os dados da tabela
+  // remove dados, exibe o loading e em seguida executa a
+  // função startUp()
+  function atualizaDados() {
+    // remove dados da tabela
+    $('#dados_locais tbody tr.linha').each(function() {
+      $(this).remove();
+    });
+    // exibe o loading
+    $('.loading').fadeIn();
+    // recarrega os dados
+    startUp();
+  }
+
+  /*
+    Inicializa os dados da tabela
+  */
+  startUp();
 
 
 
+  /*
+    Validação do Formulário
+  */
+  $('#form_adicionar').each(function() {
 
+    $('input').blur(function() {
+      if($(this).val() == "") {
+        $(this).parent().addClass('has-error');
+      } else {
+        $(this).parent().removeClass('has-error');
+      }
+    });
 
-  $('#form_adicionar #enviar').click(function() {
-
-    var formDataOK = false;
-
-    // Verificar se todos os dados foram processados
-    if($('#input_onde').value() == "") {
-      alert('Onde vazio');
-    }
-
-
-
-    // monta a variavel para ser postada para Sheetsu
-    var novaLinha = {
-        ideia: 'INSIRA AQUI SUA IDEIA',
-        categoria: 'exemplo'
-    };
-
-    if ( formDataOK ) {
-      $.ajax({
-          type: "POST",
-          url: 'https://sheetsu.com/apis/d12adf03',
-          data: novaLinha,
-          success: processarDadosPlanilha
-      });
-    }
-
-    // Função ajax que será chamada quando os dados forem recebidos do Sheetsu
-    // Caso a requisição tenha ocorrido com sucesso, faça:
-    function processarDadosPlanilha(data){
-        console.log('Dados inseridos com sucesso');
-    }
+    $('select').change(function() {
+      if($(this).val() == "") {
+        $(this).parent().addClass('has-error');
+      } else {
+        $(this).parent().removeClass('has-error');
+      }
+    });
 
   });
 
+  // Envio do formulário
+  $('#btn_submit').on('click', function(event) {
+      var $form = $("#form_adicionar");
+      var $target = $($form.attr('data-target'));
+      var dados_onde    = $('#input_onde').val();
+      var dados_local   = $('#input_local').val();
+      var dados_bairro  = $('#input_bairro').val()
+      var dados_cidade  = $('#input_cidade').val();;
+      var dados_estado  = $('#input_estado').val();;
+      var dados_wifi    = $('#select_wifi').val();
+      var dados_tomadas = $('#select_tomada').val();
+      var novaLinha = {
+        Onde    : dados_onde,
+        Local   : dados_local,
+        Bairro  : dados_bairro,
+        Cidade  : dados_cidade,
+        Estado  : dados_estado,
+        Wifi    : dados_wifi,
+        Tomadas : dados_tomadas
+      };
 
+      $.ajax({
+          type: $form.attr('method'),
+          url: $form.attr('action'),
+          dataType: "json",
+          data: $form.serialize(),
+          success: processarDadosPlanilha
+      });
+
+      // Função ajax que será chamada quando os dados forem recebidos do Sheetsu
+      // Caso a requisição tenha ocorrido com sucesso, faça:
+      function processarDadosPlanilha(data){
+          //console.log('Dados inseridos com sucesso');
+          alert('Dados submetidos com sucesso');
+          $('#formularioModal').modal('hide');
+          //console.dir(novaLinha); // DEBUG TIME!
+          atualizaDados();
+      }
+
+      event.preventDefault();
+  });
 
 });
-
-/*
-
-*/
