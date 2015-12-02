5e3423f1ed2fbdc879990c4bcc52cb227e2c5616
Felipe Fernandes <ripcorp@gmail.com>
2015-12-01 23:36:23 -0200

    adicionando validação

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,24 @@
-var app = angular.module('nomade', []);
+var app = angular.module('nomade', [
+  'jcs-autoValidate'
+]);
 
 app.controller('nomadeCtrl', function($scope, $http) {
   $scope.formModel = {};
-
+  $scope.enviando = false;
 
   $scope.onSubmit = function() {
     console.log("formulario enviado");
     console.log($scope.formModel);
+    $scope.enviando = true;
 
     $http.post('https://sheetsu.com/apis/d12adf03', $scope.formModel).
       success(function (data) {
         console.log('registrado com sucesso');
+        $scope.enviando = false;
       }).error(function (data) {
         console.log('houve uma falha no registro');
+        $scope.enviando = false;
       })
-
-
   };
 
 });
